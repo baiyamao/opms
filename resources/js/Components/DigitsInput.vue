3e685aa087d1b837134476ef4b2de3d0b7369aac
baiyamao <baiyamao@gmail.com>
2023-09-05 17:05:20 +0800

    完成数字分离输入框
    完成：
    1.阻止非数字键输入；
    2.左右方向键控制输入框；
    3.Backspace删除，自动向左移动；
    4.输入数字，自动聚焦到右边的输入框

--- resources/js/Components/DigitsInput.vue
+++ resources/js/Components/DigitsInput.vue
@@ -1,21 +1,68 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue';
 
+//使用ref保存所有输入框
+const inputsRef = ref([]);
 const digitsCount = 8;
 const digitsArray = computed(() => Array.from({ length: digitsCount }, (_, i) => i));
 const inputDigits = ref<string[]>(Array(digitsCount).fill(''));
 
 const handleInput = (index: number, event: InputEvent) => {
+    //替换非数字为空白
+    inputDigits.value[index] = inputDigits.value[index].replace(/[^0-9]/g, "");
     const nextIndex = index + 1;
     if (nextIndex < digitsCount && event.data) {
-        const inputRefName = `input${nextIndex}`;
-        const nextInput = ref(inputRefName);
-        console.log(nextInput);
-        // if (nextInput.value) {
-        //     nextInput.value.focus();
-        // }
+        const nextInput = inputsRef.value[nextIndex];
+        if (nextInput) {
+            nextInput.focus();
+        }
+    }
+    console.log(inputDigits.value);
+};
+
+const handleKeydown = (index: number, event: KeyboardEvent) => {
+    // console.log("按下的按键是：\""+event.key+"\","+inputDigits.value[index]);
+    //阻止非数字键入
+    const validKeys = [
+        "Backspace", "ArrowLeft", "ArrowRight", "Tab", "Delete"
+    ];
+    if (
+        !validKeys.includes(event.key) &&
+        (event.key < "0" || event.key > "9")
+    ) {
+        event.preventDefault();
+    }
+    if (event.key === "Backspace" && inputDigits.value[index] == "") {
+        const prevIndex = index - 1;
+        if (prevIndex >= 0) {
+            const prevInput = inputsRef.value[prevIndex];
+            if (prevInput) {
+                prevInput.focus();
+            }
+        }
+    }
+
+    if(event.key === "ArrowLeft"){
+        const prevIndex = index - 1;
+        if (prevIndex >= 0) {
+            const prevInput = inputsRef.value[prevIndex];
+            if (prevInput) {
+                prevInput.focus();
+            }
+        }
+    }
+
+    if(event.key === "ArrowRight"){
+        const nextIndex = index + 1;
+        if (nextIndex < digitsCount) {
+            const nextInput = inputsRef.value[nextIndex];
+            if (nextInput) {
+                nextInput.focus();
+            }
+        }
     }
 };
+
 
 const handlePaste = (event: ClipboardEvent) => {
     event.preventDefault();
@@ -35,9 +82,10 @@ const handlePaste = (event: ClipboardEvent) => {
             :key="index"
             v-model="inputDigits[index]"
             @input="handleInput(index, $event)"
+            @keydown="handleKeydown(index, $event)"
             @paste="handlePaste($event)"
             maxlength="1"
-            :ref="`input${index}`"
+            ref="inputsRef"
             class="px-0"
         />
     </div>
