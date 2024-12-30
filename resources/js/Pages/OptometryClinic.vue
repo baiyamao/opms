5194f292522aea25fd39f2d1de5f7bfa7fa60e7f
baiyamao <baiyamao@gmail.com>
2024-12-30 14:40:31 +0800

    添加档案窗口

--- resources/js/Pages/OptometryClinic.vue
+++ resources/js/Pages/OptometryClinic.vue
@@ -57,7 +57,7 @@ const formatDate = (dateString: string): string => {
 // 是否为多选模式
 const isMultiSelect = ref(false);
 // 单选模式下是否有条目被选中
-const isSingleSelect = ref(false);
+const isSingleSelect = ref(true);
 
 // 切换多选模式
 const toggleMultiSelect = () => {
@@ -221,6 +221,92 @@ fetchSystemAccount().then((account) => {
         console.log('未获取到账户信息');
     }
 });
+//tabs
+const activeTab = ref("overview");
+const tabs = [
+    {
+        id: "overview",
+        name: "屈光发育概况",
+        content: "屈光发育概况内容",
+        iconPath:
+            "M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z",
+        iconViewBox: "0 0 18 18",
+    },
+    {
+        id: "records",
+        name: "视光门诊记录",
+        content: "视光门诊记录内容",
+        iconPath:
+            "M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z",
+        iconViewBox: "0 0 20 20",
+    },
+    {
+        id: "eye-check",
+        name: "幼儿眼部体检",
+        content: "幼儿眼部体检内容",
+        iconPath:
+            "M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z",
+        iconViewBox: "0 0 20 20",
+    },
+    {
+        id: "vision-screening",
+        name: "学生视力筛查",
+        content: "学生视力筛查内容",
+        iconPath:
+            "M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z",
+        iconViewBox: "0 0 20 20",
+    },
+    {
+        id: "basic-info",
+        name: "档案基本信息",
+        content: "档案基本信息内容",
+        iconPath:
+            "M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z",
+        iconViewBox: "0 0 20 20",
+    },
+];
+// interface Tab {
+//     id: string;
+//     title: string;
+//     icon: string;
+// }
+//
+// const tabs = ref<Tab[]>([
+//     {
+//         id: 'overview',
+//         title: '屈光发育概况',
+//         icon: 'overview-icon' // 使用实际的图标类名替换
+//     },
+//     {
+//         id: 'records',
+//         title: '视光门诊记录',
+//         icon: 'records-icon' // 使用实际的图标类名替换
+//     },
+//     {
+//         id: 'eye-check',
+//         title: '幼儿眼部体检',
+//         icon: 'eye-check-icon' // 使用实际的图标类名替换
+//     },
+//     {
+//         id: 'vision-screening',
+//         title: '学生视力筛查',
+//         icon: 'vision-screening-icon' // 使用实际的图标类名替换
+//     },
+//     {
+//         id: 'basic-info',
+//         title: '档案基本信息',
+//         icon: 'basic-info-icon' // 使用实际的图标类名替换
+//     }
+// ]);
+//
+// const activeTab = ref(tabs.value[0].id);
+//
+// const selectTab = (tabId: string) => {
+//     activeTab.value = tabId;
+// };
+const setActiveTab = (tabId: string) => {
+    activeTab.value = tabId;
+};
 </script>
 
 
@@ -260,7 +346,7 @@ fetchSystemAccount().then((account) => {
                                 </div>
                             </div>
                         </dialog>
-                        <table class="table table-sm table-pin-rows h-full">
+                        <table class="table table-sm table-pin-rows">
                             <thead>
                             <tr>
                                 <th>
@@ -395,27 +481,110 @@ fetchSystemAccount().then((account) => {
                 v-if="isSingleSelect"
                 class="basis-2/3 max-w-7xl mx-auto sm:px-6 lg:px-8 lg:pl-2">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+
                     <div class="overflow-x-auto h-screen-minus-15 p-2">
-                        <div role="tablist" class="tabs tabs-lifted">
-                            <input type="radio" name="my_tabs_2" role="tab" class="tab" aria-label="Tab 1" />
-                            <div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6">
-                                Tab content 1
+
+                        <div class="flex flex-row justify-between items-center">
+
+                            <div class="flex flex-row items-center">
+<!--                                <div role="alert" class="alert alert-success px-1 py-0 text-sm ">-->
+<!--                                    <svg-->
+<!--                                        xmlns="http://www.w3.org/2000/svg"-->
+<!--                                        class="h-6 w-6 shrink-0 stroke-current"-->
+<!--                                        fill="none"-->
+<!--                                        viewBox="0 0 24 24">-->
+<!--                                        <path-->
+<!--                                            stroke-linecap="round"-->
+<!--                                            stroke-linejoin="round"-->
+<!--                                            stroke-width="2"-->
+<!--                                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />-->
+<!--                                    </svg>-->
+<!--                                    <span class="loading loading-bars loading-xs"></span>-->
+<!--                                    <span>正在自动建立电子档案</span>-->
+<!--                                </div>-->
+                                <div role="alert" class="alert alert-warning px-1 py-0 text-sm">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        class="h-6 w-6 shrink-0 stroke-current"
+                                        fill="none"
+                                        viewBox="0 0 24 24">
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+                                    </svg>
+                                    <span>请点击档案基本信息手动建档</span>
+                                </div>
+
+                            </div>
+                            <div class="flex flex-row gap-4 text-sm">
+                                <div class="dropdown dropdown-hover dropdown-left">
+                                    <div tabindex="0" role="button" class="link link-primary link-hover">挂号详情</div>
+                                    <div
+                                        tabindex="0"
+                                        class="dropdown-content card card-compact bg-primary text-primary-content z-[1] w-96 p-2 shadow">
+                                        <div class="card-body">
+                                            {{patientData.filter((patient) => patient.selected)}}
+                                        </div>
+                                    </div>
+                                </div>
+                                <div>纸质档案号：000100110</div>
+                                <div>电子档案号：000100110</div>
+                            </div>
+                        </div>
+                        <div>
+                            <!-- Tab Header -->
+                            <div class="border-b border-gray-200 dark:border-gray-700">
+                                <ul class="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400">
+                                    <li
+                                        v-for="tab in tabs"
+                                        :key="tab.id"
+                                        class="me-2"
+                                    >
+                                        <button
+                                            @click="setActiveTab(tab.id)"
+                                            :class="[
+                                                  'inline-flex items-center justify-center p-4 py-1 border-b-2 rounded-t-lg group',
+                                                  activeTab === tab.id
+                                                    ? 'text-blue-600 border-blue-600 dark:text-blue-500 dark:border-blue-500'
+                                                    : 'border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300'
+                                                ]"
+                                            aria-current="activeTab === tab.id ? 'page' : false"
+                                        >
+                                            <svg
+                                                class="w-4 h-4 me-2"
+                                                :class="activeTab === tab.id
+                                                ? 'text-blue-600 dark:text-blue-500'
+                                                : 'text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-gray-300'"
+                                                aria-hidden="true"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="currentColor"
+                                                :viewBox="tab.iconViewBox"
+                                            >
+                                                <path :d="tab.iconPath" />
+                                            </svg>
+                                            {{ tab.name }}
+                                        </button>
+                                    </li>
+                                </ul>
                             </div>
 
-                            <input
-                                type="radio"
-                                name="my_tabs_2"
-                                role="tab"
-                                class="tab"
-                                aria-label="挂号详情"
-                                :checked="true" />
-                            <div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6">
-                                {{patientData.filter((patient) => patient.selected)}}
-                            </div>
-
-                            <input type="radio" name="my_tabs_2" role="tab" class="tab" aria-label="Tab 3" />
-                            <div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6">
-                                Tab content 3
+                            <!-- Tab Content -->
+                            <div id="default-tab-content">
+                                <div
+                                    v-for="tab in tabs"
+                                    :key="tab.id"
+                                    :class="[
+                                              'p-4 rounded-lg bg-gray-50 dark:bg-gray-800',
+                                              activeTab === tab.id ? 'block' : 'hidden'
+                                            ]"
+                                    :id="tab.id"
+                                    role="tabpanel"
+                                    :aria-labelledby="`${tab.id}-tab`"
+                                >
+                                    {{ tab.content }}
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -425,8 +594,5 @@ fetchSystemAccount().then((account) => {
     </AuthenticatedLayout>
 </template>
 <style scoped>
-input[type="radio"]:focus {
-    outline: none;
-    box-shadow: none;
-}
+
 </style>
